9bb777fd63fdf9e4043844c4762696aeb61f8f8a
himaniasgola <62131249+himaniasgola@users.noreply.github.com>
2020-04-14 11:59:31 +0530

    Add files via upload

---
+++ src/components/User.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :myName="name" :userAge="age"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit :userAge="age"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function() {
+            return {
+                name: 'Himani',
+                age: 24
+            };
+        },
+        methods: {
+            changeName() {
+                this.name = 'Heena';
+            },
+            resetName() {
+                this.name= 'Himani';
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>UserName: {{ myName }}</p>
+        <p>UserAge: {{ userAge }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        myName: {
+            type: String
+        },
+        userAge: Number
+    } 
+}
+
+</script>
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
